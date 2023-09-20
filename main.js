50f91589dac2801362a07bf5d85b2901f1b9fe0c
Caelus111 <wmaarouf660@gmail.com>
2023-09-20 17:30:06 +0100

    nothing changed

--- main.js
+++ main.js
@@ -9,21 +9,21 @@ const thankYouCard = document.querySelector('.thank-you-state');
 
 for (let i = 0; i < ratings.length; i++) {
 
-  // Listening for a click event on each number div //
+
   ratings[i].addEventListener('click', () => {
-    // getting the clicked number value property from 1 to 5//
+
     let rating = ratings[i].innerHTML;
-    // looping through the numbers to check for a previously selected rating and removing if such. //
-    // Necessary in order to have only one rating selected at a time //
+
+    
     for (let i = 0; i < ratings.length; i++) {
       ratings[i].classList.contains('active');
       ratings[i].classList.remove('active');
     }
 
-    // add the selected class which highlights the selected rating //
+
     ratings[i].classList.add('active');
 
-    // listening for a click on the btn in order to hide the current div and display the Thank You div with the chosen rating in the span //
+
     btn.addEventListener('click', () => {
         thankYouCard.style.display = 'flex'
         validationCard.style.display = 'none'
